0a5d4b636b8784b17e5c0cd5c573ce3f449c2255
Charan968 <your-email@example.com>
2025-08-04 23:35:34 +0530

    Fix: Bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from datetime import datetime
 from reportlab.pdfgen import canvas
 import io
+import os
 
 app = Flask(__name__)
 app.secret_key = "secret123"
@@ -113,8 +114,8 @@ def logout():
     return redirect(url_for('login'))
 
 # ------------------ Start App ------------------
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
